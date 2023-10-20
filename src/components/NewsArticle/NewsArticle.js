5c346c67a5938422d86f951826732f05e1277f8f
shree1909 <snehal0108@gmail.com>
2023-10-20 14:25:48 +0530

    news app added

---
+++ src/components/NewsArticle/NewsArticle.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./NewsArticle.css";
+
+function NewsArticle({urlToImage, title, author, publishedAt, description,url}) {
+  return (
+    <div className="news-article-card">
+        <img src={urlToImage} alt="..." className="news-article-img"/>
+        <h1 className="article-title">{title}</h1>
+        <div className="article-info">
+            <p className="article-author">{author}</p>
+            <p className="article-publishedAt">{publishedAt}</p>
+        </div>
+        <p className="article-description">{description}</p>
+        <a href={url} target='_blank' className="btn-read-more">Read More</a>
+       </div>
+  )
+}
+
+export default NewsArticle
---
+++ src/views/Home/Home.js
@@ -0,0 +1,50 @@
+import React,{useState,useEffect} from 'react';
+import axios from "axios";
+import "./Home.css";
+import NewsArticle from "./../../components/NewsArticle/NewsArticle";
+
+function Home() {
+    const [news, setNews] = useState([])
+    const [searchQuery, setSearchQuery] =useState("pune")
+
+    const loadNews = async () => {
+      try {
+        const response = await axios.get(
+          `https://newsapi.org/v2/everything?q=${searchQuery}&from=2023-09-10&sortBy=publishedAt&apiKey=${process.env.REACT_APP_API_KEY}`
+        );
+        setNews(response.data.articles);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+   useEffect(() => {
+    loadNews()
+   },[])
+
+   useEffect(() =>{
+    loadNews()
+   },[searchQuery])
+   
+  return (
+    <div>
+        <h1>News App</h1>
+        <input type="text" value={searchQuery} onChange={(e) => {
+          setSearchQuery(e.target.value)
+        }}/>
+
+        <div className="news-container">
+          {
+           news.map((newsArticle, index) =>{
+           const {author, title,description, url, urlToImage, publishedAt} = newsArticle
+           return(<NewsArticle author={author} title={title} description={description} url={url} urlToImage={urlToImage}
+           publishedAt={publishedAt} key={index}/>)
+
+          })
+         }
+        </div>
+
+    </div>
+  );
+}
+
+export default Home;
